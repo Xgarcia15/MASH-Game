587cf5a1c5db9b1a823c49f57affc8170c64f87b
Xgarcia15 <70272720+Xgarcia15@users.noreply.github.com>
2020-09-10 00:24:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 function random_number(num) {
 	// Get a random number between 0 and a passed-in number
 	var num = || 4  // If no number passed in, default to 4
-	return Math.floor(Math.random() * ); // Round the answer down (floor) of a random number between 0 and 1 and multiply it by a number. Then return a value and exit the function.
+	return Math.floor(Math.random() * 4); // Round the answer down (floor) of a random number between 0 and 1 and multiply it by a number. Then return a value and exit the function.
 }
 
 // New function called mash_choice that doesn't take any parameters 
 function mash_choice() {
 	// Since MASH is a special case, give it its own list
-	var mash = ['partner', 'location', 'kids', 'car'];  // The array of choices to pick from 
+	var mash = ['mansion', 'apartment', 'shack', 'house'];  // The array of choices to pick from 
 	var randomNum = random_number(4);  // Use the above function to get a number between 0 and 4
 	return mash[randomNum];  // Return the list item the random number function just picked and exit the function 
 }
